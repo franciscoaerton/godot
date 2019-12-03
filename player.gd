f22e2377254075cd7b87e7b528a98a3ee4c8b95e
franciscoaerton <franciscoaerton@gmail.com>
2019-12-03 13:43:53 -0300

    Update player.gd

--- player.gd
+++ player.gd
@@ -6,6 +6,16 @@ extends KinematicBody2D
 # Member variables
 const MOTION_SPEED = 250 # Pixels/second
 
+var pj_st = 10
+var pj_dx = 10
+var pj_iq = 10 
+var pj_ht = 10
+
+
+var pj_dano_bal = 
+var pj_dano_gdp =
+
+
 
 func _physics_process(_delta):
 	var motion = Vector2()
@@ -24,3 +34,25 @@ func _physics_process(_delta):
 	motion = motion.normalized() * MOTION_SPEED
 
 	move_and_slide(motion)
+
+func cost_dx_and_iq(x)
+	var diferenca_x = x - 10
+	return 20 * diferenca_x
+
+func cost_st_and_ht(x)
+	var diferenca_x = x - 10
+	return 10 * diferenca_x
+	
+func dano
+	return 
+
+func matriz_dano_bal(st)
+	return 20*x
+	
+func matirz_dano_gpp(st)
+	match st:
+		1:
+			return 
+		
+	
+	
